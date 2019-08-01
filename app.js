ac1b536e3229f39da150e4fbe9d75b031ee1f0ce
Mateus Larrubia <mateus.larrubia@mercadolivre.com>
2019-08-01 16:15:51 -0300

    Add file upload

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const path = require('path');
 const meli = require('mercadolibre');
+const multer = require('multer');
+const crypto = require('crypto');
 const { validateToken } = require('./middlewares/tokens');
 const { meli_get } = require('./utils');
 const app = express();
 require('dotenv').config();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, './public/pictures'),
+  filename: (req, file, cb) => cb(null, Date.now() + file.originalname)
+});
+
+const upload = multer({ storage });
 
 const { CLIENT_ID, CLIENT_SECRET } = process.env;
 
@@ -54,7 +63,7 @@ app.get('/posts', validateToken, async (req, res) => {
 });
 
 // TODO: exemplo de post
-app.post('/post', validateToken, async (req, res) => {
+app.post('/post', validateToken, upload.single('picture'), async (req, res) => {
   try {
     const meliObject = new meli.Meli(CLIENT_ID, CLIENT_SECRET, res.locals.access_token);
     const user = await meli_get(meliObject, '/users/me');
@@ -65,14 +74,14 @@ app.post('/post', validateToken, async (req, res) => {
       price: req.body.price,
       currency_id: req.body.currency,
       available_quantity: req.body.quantity,
-      buying_mode: 'buy_it_now', // FIXME: cheque /categories/{CATEGORY_ID} nas configurações do parâmetro:{ buying_modes:[…] }.
+      buying_mode: 'buy_it_now',
       listing_type_id: req.body.listing_type,
       condition: req.body.condition,
       description: req.body.description,
       tags: [ 'immediate_payment' ],
       pictures: [
         {
-          source: 'http://mla-s2-p.mlstatic.com/968521-MLA20805195516_072016-O.jpg' // FIXME: add upload de fotos
+          source: `${req.protocol}://${req.get('host')}/pictures/${req.file.filename}`
         }
       ]
     };
@@ -80,8 +89,9 @@ app.post('/post', validateToken, async (req, res) => {
       if (err) {
         throw err;
       } else {
+        console.log('picture source:', body.pictures[0].source);
         console.log('publicado na categoria:', predict.name);
-        console.log('category probability (0-1):', predict.prediction_probability, predict.variations)
+        console.log('category probability (0-1):', predict.prediction_probability, predict.variations);
         res.send(response);
       }
     });
