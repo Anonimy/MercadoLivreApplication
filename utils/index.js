ea286104f3f0dab221d35e82d5778f505ccc118a
Mateus Larrubia <mateus.larrubia@mercadolivre.com>
2019-08-14 11:43:12 -0300

    improvements in meli utility for this project

--- utils/index.js
+++ utils/index.js
@@ -1,15 +1,36 @@
-const meli_get = (meliObject, url, params = null) => (
-  new Promise((resolve, reject) => {
-    meliObject.get(url, params, (err, result) => {
+const meli = require('mercadolibre');
+require('dotenv').config();
+
+const { CLIENT_ID, CLIENT_SECRET } = process.env;
+
+const MeliObject = function meliObject(access_token) {
+  this.meli = new meli.Meli(CLIENT_ID, CLIENT_SECRET, access_token);
+};
+
+MeliObject.prototype.get = function meli_get(url, params = null) {
+  return new Promise((resolve, reject) => {
+    this.meli.get(url, params, (err, result) => {
       if (err) {
         reject(err);
       } else {
         resolve(result);
       }
     });
-  })
-);
+  });
+};
+
+MeliObject.prototype.post = function meli_post(url, body, params = null) {
+  return new Promise((resolve, reject) => {
+    this.meli.post(url, body, params, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+}
 
 module.exports = {
-  meli_get
+  MeliObject
 };
