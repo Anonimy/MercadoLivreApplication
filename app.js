5d8dd9b10471047c7b8edd1a2bc7d66d52252596
Mateus Larrubia <mateus.larrubia@mercadolivre.com>
2019-08-14 11:49:13 -0300

    removed /posts

--- app.js
+++ app.js
@@ -105,25 +105,4 @@ app.get('/notifications', (req, res) => {
   // salvar num banco de dados de tempo real, como o firebase.
 });
 
-app.get('/posts', validateToken, async (req, res) => {
-  try {
-    const meliObject = new MeliObject(res.locals.access_token);
-    const user = await meliObject.get('/users/me');
-    const items = (await meliObject.get(`/users/${user.id}/items/search`)).results || [];
-    if (items.length) {
-      const result = [];
-      const promises = items.map(item_id => meliObject.get(`/items/${item_id}`));
-      for await (item of promises) {
-        result.push(item);
-      }
-      res.render('posts', { items: result });
-    } else {
-      res.status(404).send('no items were found :(');
-    }
-  } catch(err) {
-    console.log('Something went wrong', err);
-    res.status(500).send(`Error! ${err}`);
-  }
-});
-
 module.exports = app;
